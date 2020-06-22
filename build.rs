98da9739263720d94a0b806e86837f8278280322
Peter Marheine <peter@taricorp.net>
2020-06-22 15:27:59 +1000

    Automatically (re)build the OS image
    Might still be nice to avoid trying to build it if the binaries already
    exist, but that's harder.

--- build.rs
+++ build.rs
@@ -1,8 +1,22 @@
 fn main() {
+    rerun_if_changed("os/");
+    std::process::Command::new("make")
+        .arg("-C")
+        .arg("os")
+        .status()
+        .expect("Failed to run make to build OS");
+
+    rerun_if_changed("third_party/redcode/");
     cc::Build::new()
         .file("third_party/redcode/Z80.c")
         .define("CPU_Z80_STATIC", None)
         .define("CPU_Z80_USE_LOCAL_HEADER", None)
         .define("CPU_Z80_DEPENDENCIES_H", Some("\"z80bits.h\""))
         .compile("z80_redcode");
+}
+
+fn rerun_if_changed<P: AsRef<std::path::Path>>(path: P) {
+    for entry in walkdir::WalkDir::new(path) {
+        println!("cargo:rerun-if-changed={}", entry.unwrap().path().display());
+    }
 }
