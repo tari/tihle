291130bf65bc211b6e49621c94e42ccb7c81f0e2
Peter Marheine <peter@taricorp.net>
2020-08-07 18:45:34 +1000

    Don't always bake programs into emscripten
    build-web.sh should bake programs in, but we don't want to require it
    just when building for the emscripten target. Also share caches for the
    two emscripten builds in CI to avoid a requild when assembling the page
    package.

--- dist/build-web.sh
+++ dist/build-web.sh
@@ -6,7 +6,20 @@ OUTDIR=out/web
 rm -rf ${OUTDIR}/*
 mkdir -p ${OUTDIR}
 
+# Build programs for packaging
+make -C programs
+mkdir -p out/programs
+cp programs/*.8xp out/programs/
+
+# Quoting of link-args via environment variable is strange;
+# Cargo seems to just split on spaces, so specifying
+# "-Clink-args=--preload-file foo" passes --preload-file as
+# link-args and foo bare. Work around it by specifying link-arg
+# twice.
+PRELOAD='-Clink-arg=--preload-file -Clink-arg=out/programs/@/program/s'
+CARGO_TARGET_WASM32_UNKNOWN_EMSCRIPTEN_RUSTFLAGS="${PRELOAD}" \
+    cargo build --bin tihle --release --locked --target=wasm32-unknown-emscripten
+
 cp ${SRCDIR}/* ${OUTDIR}
-cargo build --bin tihle --release --locked --target=wasm32-unknown-emscripten
-cp target/wasm32-unknown-emscripten/release/tihle.{js,wasm} ${OUTDIR}
-cp target/wasm32-unknown-emscripten/release/deps/tihle.data ${OUTDIR}
+TARGET=target/wasm32-unknown-emscripten/release
+cp ${TARGET}/tihle.js ${TARGET}/tihle.wasm ${TARGET}/deps/tihle.data ${OUTDIR}
